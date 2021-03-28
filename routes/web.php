5f459571e1cbea93fc64ec15bca36984574d28c5
Vinicius da Silva Lourenco <vinicius.vsl@gmail.com>
2021-03-28 14:36:41 -0300

    First test and feature

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'App\Http\Controllers\ProjectsController@index');
+
+Route::post('/projects', 'App\Http\Controllers\ProjectsController@store');
