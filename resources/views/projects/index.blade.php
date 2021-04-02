ee014428aee218b583d22f41a56ca6eb8b45be2a
Vinicius da Silva Lourenco <vinicius.vsl@gmail.com>
2021-04-02 15:19:19 -0300

    Styling the project page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <header class="flex mb-3 py-4 items-center">
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h2 class="text-grey font-normal">
                 My Projects
             </h2>
@@ -16,14 +16,7 @@
     <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
             <div class="lg:w-1/3 px-3 pb-6">
-                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
-                    <h3 class="font-normal text-xl py-4 -ml-5 pl-4 mb-3 border-l-4 border-blue-light">
-                        <a class="text-black no-underline" href="{{ $project->path() }}">{{ $project->title }}</a>
-                    </h3>
-                    <div class="text-grey">
-                        {{ Str::limit($project->description, 150) }}
-                    </div>
-                </div>
+                @include('projects.card')
             </div>
         @empty
             <h3> No projects yet. </h3>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,8 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
+    <header class="flex mb-3 py-4 items-center">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-grey font-normal">
+                <a href="/projects" class="text-grey font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+            <a href="/projects/create" class="button">
+                New Project
+            </a>
+        </div>
+    </header>
 
-    <h1>{{ $project->title }}</h1>
-    <p>{{ $project->description }}</p>
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-grey font-normal text-lg mb-3">
+                        Tasks
+                    </h2>
+                    <div class="card mb-3">
+                        Lorem ipsum
+                    </div>
+                    <div class="card mb-3">
+                        Lorem ipsum
+                    </div>
+                    <div class="card mb-3">
+                        Lorem ipsum
+                    </div>
+                    <div class="card mb-3">
+                        Lorem ipsum
+                    </div>
+                </div>
+
+                <div>
+                    <h2 class="text-grey font-normal text-lg mb-3">
+                        General Notes
+                    </h2>
+                    <textarea class="card w-full" style="min-height: 150px">Lorem ipsum</textarea>
+                </div>
+            </div>
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+        </div>
+    </main>
+
+
 
 @endsection
