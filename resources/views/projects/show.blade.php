129beffcfa01d5fe8151e6917d88f9588bf6850f
Vinicius da Silva Lourenco <vinicius.vsl@gmail.com>
2021-04-21 22:11:18 -0300

    Added avatar to projects members

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,9 +6,15 @@
             <p class="text-grey font-normal">
                 <a href="/projects" class="text-grey font-normal no-underline">My Projects</a> / {{ $project->title }}
             </p>
-            <a href="{{ $project->path().'/edit' }}" class="button">
-                Edit Project
-            </a>
+            <div class="flex items-center">
+                @foreach ($project->members as $member)
+                    <img src="{{ gravatar_url($member->email) }}" alt="{{ $member->name }}'s avatar" class="rounded-full w-8 mr-2">
+                @endforeach
+                <img src="{{ gravatar_url($project->owner->email) }}" alt="{{ $project->owner->name }}'s avatar" class="rounded-full w-8 mr-2">
+                <a href="{{ $project->path().'/edit' }}" class="button ml-6">
+                    Edit Project
+                </a>
+            </div>
         </div>
     </header>
 
